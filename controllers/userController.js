ab5a8643fd9d1c1b5927f634171610c29352a879
Cassie T <cassandray9@gmail.com>
2023-05-19 17:50:04 -0600

    userController.j completed

---
+++ controllers/userController.js
@@ -0,0 +1,103 @@
+const { ObjectId } = require("mongoose").Types;
+const { User, Thought } = require("../models");
+
+module.exports = {
+  // GET all users
+  getUsers(req, res) {
+    User.find()
+      .then(async (users) => {
+        const userObj = {
+          users,
+        };
+        return res.json(userObj);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  // GET a single user by its _id and populated thought and friend data
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .then(async (user) =>
+        !user
+          ? res.status(404).json({ message: "No User with that ID" })
+          : res.json({
+              user,
+            })
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  // POST a new user:
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  // example data
+  // {
+  //     "username": "lernantino",
+  //     "email": "lernantino@gmail.com"
+  // }
+
+  // PUT to update a user by its _id
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with this id!' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+
+  // DELETE to remove user by its _id
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : Thought.deleteMany({ _id: { $in: user.thoughts } })
+      )
+      .then(() => res.json({ message: 'User and thoughts deleted!' }))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // BONUS: Remove a user's associated thoughts when deleted.
+};
+
+// /api/users/:userId/friends/:friendId
+// POST to add a new friend to a user's friend list
+// DELETE to remove a friend from a user's friend list
+
+// /api/thoughts
+// GET to get all thoughts
+// GET to get a single thought by its _id
+// POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+// example data
+// {
+//     "thoughtText": "Here's a cool thought...",
+//     "username": "lernantino",
+//     "userId": "5edff358a0fcb779aa7b118b"
+// }
+
+// PUT to update a thought by its _id
+
+// DELETE to remove a thought by its _id
+
+// /api/thoughts/:thoughtId/reactions
+
+// POST to create a reaction stored in a single thought's reactions array field
+
+// DELETE to pull and remove a reaction by the reaction's reactionId value
